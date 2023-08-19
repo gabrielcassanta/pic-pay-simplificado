96f262d51a0030781f7ffb7ae6d9ff8dca285e11
Gabriel Cassanta <cassantagabriel@gmail.com>
2023-08-19 11:02:23 -0300

    refactor: added Transaction import

--- src/main/java/com/picpaysimplificado/repositories/TransactionRepository.java
+++ src/main/java/com/picpaysimplificado/repositories/TransactionRepository.java
@@ -1,5 +1,6 @@
 package com.picpaysimplificado.repositories;
 
+import com.picpaysimplificado.picpaysimplificado.domain.transaction.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
