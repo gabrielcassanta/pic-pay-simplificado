32dab98e7e35659d0ef2c25153362c2d85a85289
Gabriel Cassanta <cassantagabriel@gmail.com>
2023-08-19 11:01:21 -0300

    refactor: added new imports

--- src/main/java/com/picpaysimplificado/repositories/UserRepository.java
+++ src/main/java/com/picpaysimplificado/repositories/UserRepository.java
@@ -1,6 +1,9 @@
 package com.picpaysimplificado.repositories;
 
+import com.picpaysimplificado.picpaysimplificado.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findUserByDocument(String document);
