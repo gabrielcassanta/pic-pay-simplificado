46948b83e8e73ad43e60c3326527878bd8b8d6b2
Gabriel Cassanta <cassantagabriel@gmail.com>
2023-08-20 18:49:24 -0300

    refactor: removed some unused imports

--- src/main/java/com/picpaysimplificado/picpaysimplificado/infra/ControllerExceptionHandler.java
+++ src/main/java/com/picpaysimplificado/picpaysimplificado/infra/ControllerExceptionHandler.java
@@ -5,7 +5,6 @@ import jakarta.persistence.EntityNotFoundException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
--- src/main/java/com/picpaysimplificado/picpaysimplificado/services/NotificationService.java
+++ src/main/java/com/picpaysimplificado/picpaysimplificado/services/NotificationService.java
@@ -2,10 +2,7 @@ package com.picpaysimplificado.picpaysimplificado.services;
 
 import com.picpaysimplificado.picpaysimplificado.domain.user.User;
 import com.picpaysimplificado.picpaysimplificado.dtos.NotificationDTO;
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
